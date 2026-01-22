3f69a2bae48230c4dabda8145f0463ec63886e66
koljastelzig <k.stelzig@campus.tu-berlin.de>
2026-01-22 21:29:59 +0100

    refactored minimal worker

--- src/zmq_worker.c
+++ src/zmq_worker.c
@@ -17,60 +17,64 @@ int main (int argc, const char * argv[])
     //  Socket to talk to clients
     void *context = zmq_ctx_new ();
 
-    int num_sockets = argc - 1;
-    void **sockets = malloc((num_sockets) * sizeof(void*));
-    for (int i=0; i<num_sockets; i++) {
-        sockets[i] = zmq_socket (context, ZMQ_REP);
-        char endpoint[64];
-        snprintf(endpoint,sizeof(endpoint), "tcp://127.0.0.1:%s",argv[i+1]);
-        int rc = zmq_bind (sockets[i], endpoint);
-        assert (rc == 0);
+    int num_ports = argc - 1;
+    void *socket = zmq_socket (context, ZMQ_REP);
+    bool bound = false;
+    for (int i=0; i<num_ports; i++) {
+        char addr[64];
+        snprintf(addr,sizeof(addr), "tcp://127.0.0.1:%s",argv[i+1]);
+        if(zmq_bind (socket, addr) == 0) {
+            bound = true;
+            break; //binded
+        };
+    }
+
+    if (!bound) {
+        perror("Worker could not bind to socket\n");
+        zmq_close(socket);
+        zmq_ctx_destroy(context);
+        return 1;
     }
 
     bool running = true;
     char buf[PROTOCOL_MAX_MSG_LEN];
 
     while (running) {
-        for (int i=0; i<num_sockets; i++) {
-            int bytes_recieved = zmq_recv(
-                sockets[i], 
-                buf, 
-                sizeof(buf)-1,
-                ZMQ_DONTWAIT);
-            if (bytes_recieved < 0) {
-                continue;
-            }
-            buf[bytes_recieved] = '\0';
-            if (!protocol_validate_message(buf)) {
-                zmq_send(sockets[i], "", 1, 0);
-                continue;
-            }
-            protocol_type_t type = protocol_get_type(buf);
+        int bytes_recieved = zmq_recv(
+            socket,
+            buf, 
+            sizeof(buf)-1,
+            0);
+        if (bytes_recieved < 0) {
+            continue;
+        }
+        buf[bytes_recieved] = '\0';
+        if (!protocol_validate_message(buf)) {
+            zmq_send(socket, "", 1, 0);
+            continue;
+        }
+        protocol_type_t type = protocol_get_type(buf);
 
-            switch (type) {
-                case PROTOCOL_MAP:
-                    zmq_send(sockets[i], "", 1,0);
-                    break;
-                case PROTOCOL_RED:
-                    zmq_send(sockets[i], "", 1,0);
-                    break;
-                case PROTOCOL_RIP:
-                    zmq_send(sockets[i], "rip", PROTOCOL_TYPE_LEN, 0);
-                    running = false;
-                    break;
+        switch (type) {
+            case PROTOCOL_MAP:
+                zmq_send(socket, "", 1,0);
+                break;
+            case PROTOCOL_RED:
+                zmq_send(socket, "", 1,0);
+                break;
+            case PROTOCOL_RIP:
+                zmq_send(socket, "rip", PROTOCOL_TYPE_LEN, 0);
+                running = false;
+                break;
 
-                default:
-                    zmq_send(sockets[i], "", 1,0);
-                    break;
-            }
+            default:
+                zmq_send(socket, "", 1,0);
+                break;
         }
     }
     //cleanup
-    for (int i=0; i<num_sockets; i++) {
-        zmq_close(sockets[i]);
-    }
+    zmq_close(socket);
     zmq_ctx_destroy(context);
-    free(sockets);
 
     return 0;
 }
